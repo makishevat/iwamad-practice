58eca9c70dfbf3935e755c66b6243caa360f4ce5
Makisheva Tomiris <t_makisheva@kbtu.kz>
2026-09-25 11:06:42 +0500

    Remove fokder and photo

--- profile-card/src/main.js
+++
@@ -1,8 +0,0 @@
-import './style.css';
-
-const btn = document.querySelector('.like-btn');
-
-btn.addEventListener('click', () => {
-  btn.classList.toggle('liked');
-  btn.textContent = btn.classList.contains('liked') ? 'Liked' : 'Like';
-});
